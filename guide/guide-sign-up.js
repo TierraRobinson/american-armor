5bfab9e9b94225fd1bef706bde6fba6ab7a4674e
Tierra Robinson <trobinson@students.hackleyschool.org>
2018-08-07 17:24:40 +0000

    first commit

---
+++ guide/guide-sign-up.js
@@ -0,0 +1,5 @@
+var background = ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgBiIWLAo8qAN_iUorHSvUyyjKgekS4nG7BTk5SDcAQ7WzTMmx', 'https://i.ytimg.com/vi/lEM2VMJmgZY/maxresdefault.jpg', 'https://static.adweek.com/adweek.com-prod/wp-content/uploads/2017/11/adobe-diversity-CONTENT-2017.jpg', 'https://3c1703fe8d.site.internapcdn.net/newman/csz/news/800/2017/psychologysd.jpg']
+
+
+
+
